55c69354627c7b5e850db9d3e30666e476a116e2
root <root@mateus-moreno.FLEX.LOCAL>
2022-12-05 12:16:23 -0300

    finished

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retornar a aplicação cli pronta para ser executada
 func Gerar() *cli.App {
@@ -8,5 +14,53 @@ func Gerar() *cli.App {
 	app.Name = "CLI App"
 	app.Usage = "Busca IPs e nomes de servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{{
+		Name:   "ip",
+		Usage:  "Busca IPs de endereços na internet.",
+		Flags:  flags,
+		Action: fetchIps,
+	},
+		{
+			Name:   "servers",
+			Usage:  "Busca o nome dos servidores",
+			Flags:  flags,
+			Action: fetchServers,
+		}}
+
 	return app
+}
+
+func fetchIps(c *cli.Context) error {
+	host := c.String("host") // retorna o valor após a flag host
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+	return nil
+}
+
+func fetchServers(c *cli.Context) error {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"ip-search/app"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Ponto de partida")
-
 	application := app.Gerar()
 	if err := application.Run(os.Args); err != nil { // permite que os comandos da linha de comando sejam interpretados e usados
 		log.Fatal(err) // Fatal() para a execução do programa.
